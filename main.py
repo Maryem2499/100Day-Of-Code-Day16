3f084b7a23d3a5abd81ce9b3317c1f168bd0260e
Maryem <61624726+Maryem2499@users.noreply.github.com>
2022-08-12 18:48:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-# # import another_module
-# # print(another_module.another_var)
-# # How can I construct a new object 'timmy' solution 1 :
-# import turtle
-# timmy = turtle.Turtle()
-# # How can I construct a new object 'timmy' solution 2 :
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# # changer la forme de visage de l'objet "timmy" a l'écran :
-# timmy.shape("turtle")
-# # changer la couleur du timmy
-# timmy.color("coral")
-# # avancement du timmy certains pas :
-# timmy.forward(250)
-# # construct an object "my_screen" et accerder a l'attribut "conveheight"
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# # construct an object "my_screen" et accerder a la methode "exitonclick()"
-# my_screen.exitonclick()
+# Importer tout les 4 classes que j' utilises apres :
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from prettytable import PrettyTable
-table=PrettyTable()
-table.add_column("Pokemon Name",["Pikatchu", "Squirtle", "Charmander"])
-table.add_column("Type",["Electric", "Water", "Fire"])
-# Change the align of the words of table right, left or center
-table.align = "l"
-print(table)
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off" :
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost) and coffee_maker.is_resource_sufficient(drink):
+                coffee_maker.make_coffee(drink)
 
 
